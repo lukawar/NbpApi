e52d15c8086809ffc73adf47e12918d09c302c07
wl_inserv <wl.inserv@gmail.com>
2020-01-08 12:51:54 +0100

    Add scripts

---
+++ NbpApiClient.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace lukawar\NbpApi;
+
+class NbpApiClient
+{
+    private $mid = 'http://api.nbp.pl/api/exchangerates/rates/a/%s/?format=json';
+    private $bidask = 'http://api.nbp.pl/api/exchangerates/rates/c/%s/?format=json';
+
+    public function getRateMid($currency)
+    {
+        $data = json_decode(file_get_contents(sprintf($this->mid, $currency)), true);
+        return ['date' => $data['rates'][0]['effectiveDate'], 'rateMid' => $data['rates'][0]['mid']];
+    }
+
+    public function getRateBidAsk($currency)
+    {
+        $data = json_decode(file_get_contents(sprintf($this->bidask, $currency)), true);
+        return ['date' => $data['rates'][0]['effectiveDate'], 'rateAsk' => $data['rates'][0]['ask'], 'rateBid' => $data['rates'][0]['bid']];
+    }
+
+    public function getRates($currency)
+    {
+        $data_mid = json_decode(file_get_contents(sprintf($this->mid, $currency)), true);
+        $data_bidask = json_decode(file_get_contents(sprintf($this->bidask, $currency)), true);
+        return [
+            'date' => $data_mid['rates'][0]['effectiveDate'],
+            'rateMid' => $data_mid['rates'][0]['mid'],
+            'rateAsk' => $data_bidask['rates'][0]['ask'],
+            'rateBid' => $data_bidask['rates'][0]['bid']
+        ];
+    }
+}
